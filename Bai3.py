aa6fa39297b97f75c131ce277456f7ddc182dd7d
Viet7501 <94689163+Viet7501@users.noreply.github.com>
2021-11-19 19:21:28 +0700

    Add files via upload

---
+++ Bai3.py
@@ -0,0 +1,5 @@
+name = str(input("Tên bạn là: "))
+print(f'Chào mừng {name} đã đến chương trình')
+
+nam_sinh = int(input("Vui lòng nhập năm sinh: "))
+print(f'Hiện tại bạn: {2021 - nam_sinh} tuổi.',)
