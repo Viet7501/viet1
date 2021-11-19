bb0926194cba85d6ebaae02ab24ffb845e73d840
Viet7501 <94689163+Viet7501@users.noreply.github.com>
2021-11-19 19:14:20 +0700

    Add files via upload

---
+++ Bai2.py
@@ -0,0 +1,32 @@
+# Theo bài học có 4 kiểu dữ liệu
+    # bool
+    # dữ liệu số: số nguyên(int), số thực(float), số phức(complex)
+    # Ký tự(Characters), xâu ký tự (string)
+    # sequence, set,
+
+a = True
+b = False
+print(a, b)
+
+c = 10
+d = 20
+print(c, d)
+
+e = 10.5
+f = 1.667
+print(e, f)
+
+g = 1+2j
+h = 2-3j
+print(g, h)
+
+name = 'Viet'
+nghe_nghiep = 'Sinh vien'
+print(name, nghe_nghiep, sep='\n')
+
+lst = [1, 2, 3, 4]
+ten = ["Viet","Trang", "Linh", "Hung"]
+print(lst)
+print(ten)
+
+
