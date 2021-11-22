92ed032f3fcf8f10899b12eaf8d106d2ef9be857
Viet7501 <94689163+Viet7501@users.noreply.github.com>
2021-11-22 22:00:49 +0700

    Update Bai3.py

--- Bai3.py
+++ Bai3.py
@@ -2,4 +2,4 @@ name = str(input("Tên bạn là: "))
 print(f'Chào mừng {name} đã đến chương trình')
 
 nam_sinh = int(input("Vui lòng nhập năm sinh: "))
-print(f'Hiện tại bạn: {2021 - nam_sinh} tuổi.',)
+print(f'Hiện tại bạn: {2021 - nam_sinh} tuổi.')
