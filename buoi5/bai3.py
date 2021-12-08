1b76064e0303b72d2dd708794d8e65caf3fde7e6
Viet7501 <94689163+Viet7501@users.noreply.github.com>
2021-12-08 19:50:47 +0700

    Update bai3.py

--- buoi5/bai3.py
+++ buoi5/bai3.py
@@ -1,17 +1,13 @@
 'Viết chương trình tạo ra list mới bằng cách ghép một chuỗi s vào các phần tử list cũ'
 
 n = int(input('Nhập độ dài list: '))
-s = []
-s_1 = []
+the_list = []
 for i in range(n):
-    s.append(int(input(f's[{i}]: ')))
+    the_list .append(input(f's[{i}]: '))
 
-print('List s cần ghép là:')
-m = int(input('Nhập độ dài list: '))
-for i in range(m):
-    s_1.append(int(input(f's_1[{i}]: ')))
+s = input("Nhập chuỗi s: ")
 
-s.extend(s_1)
-
-
-print(f'List ban đầu được ghép thành: {s}')
+new_list = []
+for item in the_list:
+    new_list.append(s+item)
+print(f'List mới: {new_list}')
