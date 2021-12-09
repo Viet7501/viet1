b6631bccc2f4bf4416c455a88a364e728bd36e2a
Viet7501 <94689163+Viet7501@users.noreply.github.com>
2021-12-09 19:39:07 +0700

    Update bai12.py

--- buoi7/bai12.py
+++ buoi7/bai12.py
@@ -1,10 +1,11 @@
 set1 = {0, 1, 2, 3, 4, 5, 6}
 set2 = {5, 6, 7, 8, 9, 0, 1}
 
-
-print(set1.difference_update(set2))
-print(set1.intersection_update(set2))
-print(set1.isdisjoint(set2))
+# set1.difference_update(set2)
+# set1.intersection_update(set2)
 print(set1.issubset(set2))
+print(set1.isdisjoint(set2))
 print(set1.issuperset(set2))
-print(set1.symmetric_difference_update(set2))
+# set1.symmetric_difference_update(set2)
+
+print(set1)
