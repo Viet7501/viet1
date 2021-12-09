6e397851511f0e04e1f008988204ee1618cc0fcf
Viet7501 <94689163+Viet7501@users.noreply.github.com>
2021-12-09 19:39:20 +0700

    Update bai12.py

--- buoi7/bai12.py
+++ buoi7/bai12.py
@@ -6,6 +6,6 @@ set2 = {5, 6, 7, 8, 9, 0, 1}
 print(set1.issubset(set2))
 print(set1.isdisjoint(set2))
 print(set1.issuperset(set2))
-# set1.symmetric_difference_update(set2)
+set1.symmetric_difference_update(set2)
 
 print(set1)
