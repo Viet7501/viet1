cb2a66f51e55a10d56434b4927605ba79e9381ec
Viet7501 <94689163+Viet7501@users.noreply.github.com>
2021-12-15 21:11:19 +0700

    Update bai9.py

--- buoi9/bai9.py
+++ buoi9/bai9.py
@@ -3,28 +3,16 @@ Viết hàm đệ quy đếm và trả về số lượng chữ số lẻ của 
 Ví dụ: Hàm trả về 4 nếu n là 19922610 (do n có 4 số lẻ là 1, 9, 9, 1)
 """
 
-def dem_so_le(n):
-    so_le = 0
-    while n > 0:
-        num = list(str(n))
-        for i in num:
-            if i in ('1', '3', '5', '7', '9'):
-                so_le += 1
-        return so_le
 
-print(dem_so_le(123))
-print(dem_so_le(19922610))
+def demsole(n):
 
-# def countOdd(n):
-#     so_le = 0
-#
-#     while (n > 0):
-#         rem = n % 10
-#         if (rem % 2 != 0):
-#             so_le += 1
-#
-#         n = int(n / 10)
-#
-#     return so_le
-#
-# print(countOdd(1235))
+    if n == 0:
+        return 0
+    else:
+        if n % 2 != 0:
+            return 1 + demsole(n // 10)
+        return demsole(n // 10)
+
+
+print(demsole(1235))
+print(demsole(19922610))
